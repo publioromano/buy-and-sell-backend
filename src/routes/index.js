155f7614be9621545b22105d0476c02584b8a99b
publio.carreiro <publio.carreiro@siemens.com>
2022-04-13 12:11:14 +0100

    CRUD routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { addViewToListRoute } from "./addViewToListing";
-
+import { getUserListingsRoute } from "./getUserListings";
+import { createNewListingRoute } from "./createNewListing";
+import { updatedListingRoute } from "./updateListing";
+import { deleteListingRoute } from "./deleteListing";
 export default [
+    deleteListingRoute,
+    updatedListingRoute,
+    createNewListingRoute,
+    getUserListingsRoute,
     getAllListingsRoute,
     getListingRoute,
     addViewToListRoute
