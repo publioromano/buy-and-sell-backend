c17e9314e929a587ee1883685013372fac9a1f86
publio.carreiro <publio.carreiro@siemens.com>
2022-04-19 12:36:26 +0100

    all full stack created

--- src/routes/getAllListings.js
+++ src/routes/getAllListings.js
@@ -4,7 +4,7 @@ export const getAllListingsRoute = {
     method: 'GET',
     path: '/api/listings',
     handler: async (req, h) => {
-        const result = await db.query('SELECT * FROM listings');
-        return result;
+        const { results } = await db.query('SELECT * FROM listings');
+        return results;
     }
 }
