2d73d68637dcdaf797c57047a00a30f5401c9269
publio.carreiro <publio.carreiro@siemens.com>
2022-04-26 11:29:00 +0100

    finished

--- src/routes/createNewListing.js
+++ src/routes/createNewListing.js
@@ -1,4 +1,5 @@
 import { v4 as uuid } from 'uuid';
+import * as admin from 'firebase-admin';
 import { db } from '../database';
 import { getUserListingsRoute } from './getUserListings';
 
@@ -6,9 +7,11 @@ export const createNewListingRoute = {
     method: 'POST',
     path: '/api/listings',
     handler: async (req, h) => {
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
+        const userId = user.user_id;
         const id = uuid();
         const { name = '', description = '', price = 0} = req.payload;
-        const userId = '12345';
         const  views = 0;
 
         await db.query(`
--- src/routes/getUserListings.js
+++ src/routes/getUserListings.js
@@ -1,10 +1,16 @@
+import { Boom } from '@hapi/boom';
+import * as admin from 'firebase-admin';
 import { db } from "../database";
 
 export const getUserListingsRoute = {
     method: 'GET',
     path: '/api/users/{userId}/listings',
     handler: async (req, h) => {
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
         const userId = req.params.userId;
+
+        if (user.user_id !== userId) throw Boom.unauthorized('Users can only acces their own listings!');
 
         const { results } = await db.query('SELECT * FROM listings WHERE user_id = ?',  [userId]);
 
